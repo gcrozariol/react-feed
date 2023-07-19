16969a0d80cc732f250b176d31ffdac216887ed1
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2023-07-19 12:21:59 -0300

    Separate avatar component for reuse + responsive layout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar'
 
 export function Sidebar() {
   return (
@@ -9,7 +10,7 @@ export function Sidebar() {
         src='https://images.unsplash.com/photo-1689746454688-a6566687a86e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50' />
       
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/gcrozariol.png" />
+        <Avatar src="https://github.com/gcrozariol.png" />
         <strong>Guilherme Crozariol</strong>
         <span>Fullstack Developer</span>
       </div>
