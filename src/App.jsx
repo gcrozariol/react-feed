fcf4f3bb73fc445ffc992c6e3bcc5b2a4b2b7cc0
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2023-07-19 08:46:31 -0300

    Add sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { Post } from "./Post"
 import { Header } from "./components/Header"
+
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post key={1} author="Guilherme Crozariol" content="This is a post content!" />
-      <Post key={1} author="Claudio Crozariol" content="This is a another post content!" />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post key={1} author="Guilherme Crozariol" content="This is a post content!" />
+          <Post key={1} author="Claudio Crozariol" content="This is a another post content!" />
+        </main>
+      </div>
     </div>
   )
 }
